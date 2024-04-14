a0f3093c96c55e624ef071be6199c260d3f1a209
vivvy-snow <42851127+vivvy-snow@users.noreply.github.com>
2024-04-13 17:43:51 -0700

    change/fix variable and file names (#4)
    * change variables for readability
    
    * fix variable and file names

--- findInteractables.py
+++ findInteractables.py
@@ -14,24 +14,22 @@ def is_admin():
 current_dir = os.getcwd()
 
 # Create a relative path to the folder with images
-slugPath = os.path.join(current_dir, 'images', 'ingame_slug.png')
-jarFairyPath = os.path.join(current_dir, 'images', 'jarfairy.png')
-giftPath = os.path.join(current_dir, 'images', 'giftBubble.png')
-cameraPath = os.path.join(current_dir, 'images', 'camera.png')
-menuPath = os.path.join(current_dir, 'images', 'menuRed.png')
-frogPath = os.path.join(current_dir, 'images', 'frog.png')
-balloonPath = os.path.join(current_dir, 'images', 'balloon.png')
+slug_path = os.path.join(current_dir, 'images', 'slug.png')
+frog_path = os.path.join(current_dir, 'images', 'frog.png')
+jar_fairy_path = os.path.join(current_dir, 'images', 'jar_fairy.png')
+gift_path = os.path.join(current_dir, 'images', 'gift.png')
+butterfly_path = os.path.join(current_dir, 'images', 'butterfly.png')
+upgrade_path = os.path.join(current_dir, 'images', 'upgrade.png')
 
 # Active state
 active = True
 
 # Counters for all the interactables
-slugsClaimed = 0
-jarsClaimed = 0
-giftsClaimed = 0    
-camerasClaimed = 0
-frogsClaimed = 0
-balloonsClaimed = 0
+slugs_claimed = 0
+jars_claimed = 0
+gifts_claimed = 0
+frogs_claimed = 0
+butterflies_claimed = 0
 
 def main():
     
@@ -43,43 +41,43 @@ def main():
     try:
         while is_admin() and active:
             # Find slug by image that is provided and click it
-            findSlug = py.locateCenterOnScreen(slugPath, confidence=0.9)
-            if findSlug is not None:
-                py.click(findSlug)
+            find_slug = py.locateCenterOnScreen(slug_path, confidence=0.9)
+            if find_slug is not None:
+                py.click(find_slug)
+                time.sleep(3)
+                py.click(x=1280, y=715)
                 time.sleep(1)
-                py.click(x=960, y=523)
-                time.sleep(1)
-                py.click(x=960, y=604)
-                global slugsClaimed
-                slugsClaimed+=1
+                py.click(x=1280, y=807)
+                global slugs_claimed
+                slugs_claimed+=1
 
             # Find frog by image, click it and get rewards
-            findFrog = py.locateCenterOnScreen(frogPath, confidence=0.9)
-            if findFrog is not None:
+            find_frog = py.locateCenterOnScreen(frog_path, confidence=0.9)
+            if find_frog is not None:
                 time.sleep(1)
-                py.click(findFrog)
+                py.click(find_frog)
+                time.sleep(3)
+                py.click(x=1280, y=715)
                 time.sleep(1)
-                py.click(x=960, y=523)
-                time.sleep(1)
-                py.click(x=960, y=604)
-                global frogsClaimed
-                frogsClaimed+=1
+                py.click(x=1280, y=807)
+                global frogs_claimed
+                frogs_claimed+=1
 
             # Find Jar Fairy by image, click it and get rewards
-            findJarFairy = py.locateCenterOnScreen(jarFairyPath, confidence=0.9)
-            if findJarFairy is not None:
+            find_jar_fairy = py.locateCenterOnScreen(jar_fairy_path, confidence=0.9)
+            if find_jar_fairy is not None:
                 time.sleep(0.3)
-                py.click(findJarFairy)
+                py.click(find_jar_fairy)
                 time.sleep(0.3)
-                py.click(x=997, y=711)
-                global jarsClaimed
-                jarsClaimed+=1
+                py.click(x=1323, y=944)
+                global jars_claimed
+                jars_claimed+=1
 
             # Find gift by image that is provided and click said gift
-            findGift = py.locateCenterOnScreen(giftPath, confidence=0.9)
-            if findGift is not None:
+            find_gift = py.locateCenterOnScreen(gift_path, confidence=0.9)
+            if find_gift is not None:
                 time.sleep(0.3)
-                py.click(findGift)
+                py.click(find_gift)
                 time.sleep(5)
                 # Click 1/3 gift (middle gift)
                 py.click(x=957, y=628)
@@ -88,35 +86,56 @@ def main():
                 py.click(x=957, y=775)
                 # Click again to exit gift menu
                 time.sleep(1)
-                py.click(x=957, y=775)
-                global giftsClaimed
-                giftsClaimed+=1
+                py.click(x=1280, y=1030)
+                global gifts_claimed
+                gifts_claimed+=1
 
-            # Find camera by image, click it and get rewards
-            findCamera = py.locateCenterOnScreen(cameraPath, confidence=0.95)
-            if findCamera is not None:
-                time.sleep(0.3)
-                py.click(findCamera)
-                time.sleep(0.3)
-                py.click(x=956, y=865)
+            # Find butterfly by image, click it and get rewards
+            find_butterfly = py.locateCenterOnScreen(butterfly_path, confidence=0.95)
+            if find_butterfly is not None:
+                time.sleep(0.5)
+                py.click(find_butterfly)
+                time.sleep(3)
+                py.click(x=1280, y=715)
                 time.sleep(1)
-                py.click(x=956, y=865)
-                global camerasClaimed
-                camerasClaimed+=1
+                py.click(x=1280, y=807)
+                butterflies_claimed+=1
 
-            # Refresh Herb Stand and Cook Library buffs
-            refreshFoodBuff = py.locateCenterOnScreen(menuPath, confidence=0.9)
-            if refreshFoodBuff is not None:
-                time.sleep(0.3)
-                py.click(refreshFoodBuff)
-                time.sleep(0.3)
-                # Refresh Herb Stand buff
-                py.click(x=857, y=663)
+            # Function to hold mouse click until image disappears
+            def hold_mouse_until_image_gone(image_path):
+                # Find the image on the screen
+                image_location = py.locateOnScreen(image_path, confidence=0.9)
 
-                py.click(refreshFoodBuff)
-                time.sleep(0.3)
-                # Refresh Cook Library buff
-                py.click(x=961, y=653)
+                # If the image is found
+                if image_location is not None:
+                    # Get the center coordinates of the image
+                    image_center = py.center(image_location)
+                    
+                    # Simulate moving the mouse to the center of the image
+                    py.moveTo(image_center)
+                    
+                    # Simulate pressing down the mouse button
+                    py.mouseDown()
+
+                    # Loop until the image is no longer found
+                    while True:
+                        # Find the image on the screen again within a region around the original location
+                        image_location = py.locateOnScreen(image_path, confidence=0.9, region=image_location)
+                        
+                        # If the image is not found, exit the loop
+                        if image_location is None:
+                            break
+
+                        time.sleep(0.1)  # Adjust sleep duration as needed
+
+                    # Release the mouse button
+                    py.mouseUp()
+                else:
+                    # Handle case when image is not found
+                    pass
+
+            # Call the function with the relative path to the image
+            hold_mouse_until_image_gone(upgrade_path)
 
     # End result of how long the script was running for and how many interactables were clicked aka claimed
     except KeyboardInterrupt:
@@ -132,7 +151,7 @@ def main():
     
         print("You have been searching for interactables for: {:.0f} hours, {:.0f} minutes, {:.2f} seconds".format(hours, minutes, seconds))
 
-        counters = {"Slugs": slugsClaimed, "Jars": jarsClaimed, "Gifts": giftsClaimed, "Cameras": camerasClaimed, "Frogs": frogsClaimed}
+        counters = {"Slugs": slugs_claimed, "Jars": jars_claimed, "Gifts": gifts_claimed, "Frogs": frogs_claimed, "Butterflies ": butterflies_claimed}
 
         for message, counter in counters.items():
             print(f"{message}: {counter}")
